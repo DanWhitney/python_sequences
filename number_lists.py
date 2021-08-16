9693969e4f9413c2cb6deef3bb9ff5d89f66d35d
Dan Whitney <dwhitney197@gmail.com>
2021-08-16 21:08:01 +0100

    up to 104

--- number_lists.py
+++ number_lists.py
@@ -1,18 +1,42 @@
+empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-even.extend(odd)
+numbers = even + odd
+print(numbers)
 
-print(even)
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+print(numbers)
 
-another_even = even
+digits = sorted("432985617")
+print(digits)
 
-print(another_even)
+new_digits = list("432985617")
+print(new_digits)
 
-even.sort(reverse=True)
+#more_numbers = list(numbers) #copy list
+#more_numbers = numbers[:]
+more_numbers = numbers.copy()
+print(more_numbers)
 
-print(even)
-print(another_even)
+print(numbers is more_numbers)
+
+print(numbers == more_numbers)
+
+
+# even.extend(odd)
+#
+# print(even)
+#
+# another_even = even
+#
+# print(another_even)
+#
+# even.sort(reverse=True)
+#
+# print(even)
+# print(another_even)
 # print(min(even))
 # print(max(even))
 #
