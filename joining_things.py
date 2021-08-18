68066a60d2540fe8fbcb0ceed8804616c9a68e1c
Dan Whitney <dwhitney197@gmail.com>
2021-08-18 08:30:56 +0100

    up to 118

---
+++ joining_things.py
@@ -0,0 +1,17 @@
+flowers = [
+    "Daffodil",
+    "Evening Primrose",
+    "Hydrangea",
+    "Iris",
+    "lavender",
+    "Sunflower",
+    "Tiger lily"
+]
+
+# for flower in flowers:
+#     print(flower)
+
+separator = ":\n "
+output = separator.join(flowers)
+
+print(output)
--- no_spam.py
+++ no_spam.py
@@ -8,3 +8,25 @@ menu = [
     ["spam", "egg", "spam", "spam", "bacon", "spam"],
     ["spam", "sausage", "spam", "bacon", "spam", "tomato", "spam"],
 ]
+
+# reverse removal
+#
+# for index1, dish in enumerate(menu):
+#     mumberOfItemsInDish = len(dish)
+#     print(dish)
+#     for index2, item in enumerate(reversed(dish)):
+#         if item == "spam":
+#             positionToDelete = (mumberOfItemsInDish - 1) - index2
+#             print(positionToDelete)
+#             del menu[index1][positionToDelete]
+#
+# print(menu)
+
+
+for meal in menu:
+    for index in range(len(meal) - 1, -1, -1):
+        if meal[index] == "spam":
+            del meal[index]
+
+    print(", ".join(meal))
+
